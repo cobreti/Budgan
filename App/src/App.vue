14f87fcaf823ded4a241c734c9caf6cfeeb4c4a9
Danny Thibaudeau <cobreti_@hotmail.com>
2026-03-07 18:40:01 -0500

    Transfered files from Budgan-wui

---
+++ App/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-app :full-height="true">
+      <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random" title="Budgan">
+        <template v-slot:image>
+          <v-img
+              gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click.stop="onToggleDrawer"></v-app-bar-nav-icon>
+        </template>
+        <template v-slot:append>
+          v{{version}}
+        </template>
+      </v-app-bar>
+      <v-main :scrollable="false">
+        <RouterView>
+        </RouterView>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { useAppSettingsStore } from '@/stores/appSettings-store'
+import settings from './assets/settings.json';
+import { computed } from 'vue'
+
+const appSettingsStore = useAppSettingsStore();
+
+appSettingsStore.setVersion(settings.version);
+
+const version = computed(() => appSettingsStore.appSettings.version);
+
+</script>
---
+++ App/src/main.ts
@@ -0,0 +1,52 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { createPinia } from 'pinia'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import { container } from './core/setupInversify'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
+
+app.provide('container', container)
+
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            dark: {
+                colors: {
+                    // "primary": colors.blue.darken3,
+                    // "secondary": colors.cyan.accent1,
+                    // "surface": colors.lightBlue.darken4
+                }
+            }
+        }
+    },
+    icons: {
+        defaultSet: 'mdi'
+    },
+    components,
+    directives
+})
+
+app.use<any>(vuetify, {
+    options: {
+        customProperties: true
+    }
+})
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
