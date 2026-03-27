20d857818210a0d6edfc64708c066dd199b7c55c
Danny Thibaudeau <cobreti_@hotmail.com>
2026-03-27 17:22:47 -0400

    added workspace view

--- App/src/engine-testapp/components/navigation-bar/navigation-bar.vue
+++ App/src/engine-testapp/components/navigation-bar/navigation-bar.vue
@@ -1,39 +1,40 @@
 <template>
-    <div class="menu">
-        <a :href="`/${locale}/`">{{ t('navbar.home') }}</a>
-        <a :href="`/${locale}/zip-file`">{{ t('navbar.zipfile' )}}</a>
-    </div>
+  <div class="menu">
+    <a :href="`/${locale}/`">{{ t('navbar.home') }}</a>
+    <a :href="`/${locale}/zip-file`">{{ t('navbar.zipfile') }}</a>
+    <a :href="`/${locale}/workspace`">{{ t('navbar.workspace') }}</a>
+  </div>
 </template>
 
 <script setup lang="ts">
-    import type { NavigationBarProps } from './types.ts'
-    import { useI18n } from 'vue-i18n'
+  import type { NavigationBarProps } from './types.ts'
+  import { useI18n } from 'vue-i18n'
 
-    const { locale, t } = useI18n()
-    const props = defineProps<NavigationBarProps>()
+  const { locale, t } = useI18n()
+  const props = defineProps<NavigationBarProps>()
 </script>
 
 <style scoped>
-    @use 'colors-def';
+  @use 'colors-def';
 
-    .menu {
-        display: flex;
-        flex-direction: row;
-        gap: 20px;
-        background-color: var(--app-nav-background);
-        color: var(--app-nav-active-menu-item);
-        font-family: 'Inter', sans-serif;
-        font-size: 14px;
+  .menu {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    background-color: var(--app-nav-background);
+    color: var(--app-nav-active-menu-item);
+    font-family: 'Inter', sans-serif;
+    font-size: 14px;
 
-        a {
-            color: var(--app-nav-active-menu-item);
-            text-decoration: none;
-            cursor: pointer;
-        }
-
-        a:hover {
-            color: var(--app-nav-active-menu-item-selected);
-            text-decoration: underline;
-        }
+    a {
+      color: var(--app-nav-active-menu-item);
+      text-decoration: none;
+      cursor: pointer;
     }
+
+    a:hover {
+      color: var(--app-nav-active-menu-item-selected);
+      text-decoration: underline;
+    }
+  }
 </style>
--- App/src/engine-testapp/router/index.ts
+++ App/src/engine-testapp/router/index.ts
@@ -1,30 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@engineTestAppViews/Home/home.vue';
+import HomeView from '@engineTestAppViews/Home/home.vue'
 import { setupRouteTracking } from '@engineTestAppRouter/routeTracker.ts'
 import Layout from '@engineTestAppViews/layout.vue'
 import ZipFile from '@engineTestAppViews/zip-file/zip-file.vue'
 
 export const router = createRouter({
-
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/:locale(en|fr)',
+      component: Layout,
+      children: [
         {
-            path: '/:locale(en|fr)',
-            component: Layout,
-            children: [
-                {
-                    path: '',
-                    name: 'home',
-                    component: HomeView
-                },
-                {
-                    path: 'zip-file',
-                    name: 'zip-file',
-                    component: () => import('@engineTestAppViews/zip-file/zip-file.vue')
-                }
-            ]
+          path: '',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: 'zip-file',
+          name: 'zip-file',
+          component: () => import('@engineTestAppViews/zip-file/zip-file.vue')
+        },
+        {
+          path: 'workspace',
+          name: 'workspace',
+          component: () => import('@engineTestAppViews/workspace/workspace.vue')
         }
-    ]
-});
+      ]
+    }
+  ]
+})
 
-setupRouteTracking(router);
+setupRouteTracking(router)
