7bf64299723589c12276a1797e9629c654c63844
Danny Thibaudeau <cobreti_@hotmail.com>
2026-03-30 19:46:38 -0400

    added draft add account page

---
+++ App/src/engine-testapp/views/workspace/account-add.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="account-add">
+    <h3 class="account-add__title">{{ t('workspace.menu.addAccount') }}</h3>
+    <p class="account-add__description">This is a placeholder for adding an account to the workspace.</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+
+  const { t } = useI18n()
+</script>
+
+<style scoped>
+  .account-add {
+    display: grid;
+    gap: 1.5rem;
+  }
+
+  .account-add__title {
+    margin: 0;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+
+  .account-add__description {
+    margin: 0;
+    color: rgb(var(--v-theme-on-surface-variant));
+  }
+</style>
