a402664d7688a44cdc26faee285866fa2227f37a
Danny Thibaudeau <cobreti_@hotmail.com>
2026-04-06 12:57:05 -0400

    cleanup

--- App/src/engine-testapp/router/index.ts
+++ App/src/engine-testapp/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@engineTestAppViews/Home/home.vue'
 import { setupRouteTracking } from '@engineTestAppRouter/routeTracker.ts'
 import Layout from '@engineTestAppViews/layout.vue'
-import ZipFile from '@engineTestAppViews/zip-file/zip-file.vue'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
