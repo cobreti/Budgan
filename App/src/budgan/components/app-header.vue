204bafbb0e0eb9b3d1fd2c553083bcf05e73d86d
Danny Thibaudeau <cobreti_@hotmail.com>
2026-04-25 09:36:47 -0400

    added hamburger menu

--- App/src/budgan/components/app-header.vue
+++ App/src/budgan/components/app-header.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app-bar color="primary">
+    <template #prepend>
+      <v-app-bar-nav-icon
+        class="app-header__menu-btn"
+        data-testid="app-header-menu-btn"
+        aria-label="Open menu"
+      />
+    </template>
     <template #title>
       <span class="app-header__title">Budgan</span>
     </template>
@@ -12,6 +19,10 @@
 .app-header__title {
   cursor: default;
   user-select: none;
+}
+
+.app-header__menu-btn {
+  cursor: pointer;
 }
 </style>
 
