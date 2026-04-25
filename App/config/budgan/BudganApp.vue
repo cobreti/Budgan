9e7e11c581a10055d0493738580efc9aacc5cef6
Danny Thibaudeau <cobreti_@hotmail.com>
2026-04-25 09:14:02 -0400

    moved header into a component added missing file from the merge

--- App/config/budgan/BudganApp.vue
+++ App/config/budgan/BudganApp.vue
@@ -1,22 +1,9 @@
 <template>
   <div>
     <v-app :full-height="true">
-      <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random" title="Budgan">
-        <template v-slot:image>
-          <v-img
-              gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-          ></v-img>
-        </template>
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-        <template v-slot:append>
-          v{{version}}
-        </template>
-      </v-app-bar>
+      <AppHeader />
       <v-main :scrollable="false">
-        <RouterView>
-        </RouterView>
+        <RouterView />
       </v-main>
     </v-app>
   </div>
@@ -29,13 +16,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { useAppSettingsStore } from '@/stores/appSettings-store.ts'
-import settings from '@/assets/settings.json';
-import { computed } from 'vue'
+import settings from '@/assets/settings.json'
+import AppHeader from '@/components/app-header.vue'
 
-const appSettingsStore = useAppSettingsStore();
-
-appSettingsStore.setVersion(settings.version);
-
-const version = computed(() => appSettingsStore.appSettings.version);
-
+const appSettingsStore = useAppSettingsStore()
+appSettingsStore.setVersion(settings.version)
 </script>
---
+++ App/src/budgan/components/app-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar color="teal-darken-4" title="Budgan">
+    <template #append>
+      v{{ version }}
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useAppSettingsStore } from '@/stores/appSettings-store.ts'
+
+const appSettingsStore = useAppSettingsStore()
+const version = computed(() => appSettingsStore.appSettings.version)
+</script>
+
