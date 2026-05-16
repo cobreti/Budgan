6ccc4b10cf42b955d5299692c892bcb1ae79aa27
Danny Thibaudeau <cobreti_@hotmail.com>
2026-05-16 15:12:01 -0400

    update main menu button with localization

--- App/budgan/src/components/main-menu/main-menu.component.ts
+++ App/budgan/src/components/main-menu/main-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { MainMenuButtonComponent } from './main-menu-button/main-menu-button.component';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
-  imports: [MainMenuButtonComponent]
+  imports: [MainMenuButtonComponent, TranslatePipe],
 })
 export class MainMenuComponent { }
