135c52f3b8ce671d867ea118baf919b60fd12b0a
Danny Thibaudeau <cobreti_@hotmail.com>
2026-05-17 17:53:09 -0400

    added menu on home page

--- App/budgan/src/components/home/home.component.ts
+++ App/budgan/src/components/home/home.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LOCALE_SERVICE, LocaleService } from '../../services/locale.service';
+import { MainMenuButtonComponent } from '../main-menu/main-menu-button/main-menu-button.component';
 
 @Component({
   selector: 'home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.scss']
+  styleUrls: ['home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MainMenuButtonComponent, TranslatePipe],
 })
-export class HomeComponent { }
+export class HomeComponent {
+  private readonly _router = inject(Router);
+  private readonly _locale = inject<LocaleService>(LOCALE_SERVICE);
 
+  onNewJournal(): void {
+    this._router.navigate([this._locale.currentLocale(), 'journal', 'new']);
+  }
+}
