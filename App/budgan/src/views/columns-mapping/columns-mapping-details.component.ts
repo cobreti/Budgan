33c08edec1052278ccb788dad30d79d9112f4a54
Danny Thibaudeau <cobreti_@hotmail.com>
2026-05-23 12:41:01 -0400

    implented columns mapping details

--- App/budgan/src/views/columns-mapping/columns-mapping-details.component.ts
+++ App/budgan/src/views/columns-mapping/columns-mapping-details.component.ts
@@ -1,8 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ColumnsMapping } from '../../Models/ColumnsMappingModel';
+import { COLUMNS_MAPPING_SERVICE, ColumnsMappingService } from '../../services/columns-mapping.service';
+import { LOCALE_SERVICE, LocaleService } from '../../services/locale.service';
+import { PageMenuComponent } from '../../components/page-menu/page-menu.component';
+import { PageMenuButtonComponent } from '../../components/page-menu/page-menu-button/page-menu-button.component';
 
 @Component({
   selector: 'app-columns-mapping-details',
-  template: '',
+  templateUrl: './columns-mapping-details.component.html',
+  styleUrl: './columns-mapping-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TranslatePipe, PageMenuComponent, PageMenuButtonComponent],
 })
-export class ColumnsMappingDetailsComponent {}
+export class ColumnsMappingDetailsComponent implements OnInit {
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _router = inject(Router);
+  private readonly _locale = inject<LocaleService>(LOCALE_SERVICE);
+  private readonly _service = inject<ColumnsMappingService>(COLUMNS_MAPPING_SERVICE);
+
+  readonly mapping = signal<ColumnsMapping | undefined>(undefined);
+
+  async ngOnInit(): Promise<void> {
+    const id = this._route.snapshot.params['columnsMappingId'];
+    const entry = await this._service.getById(id);
+    this.mapping.set(entry);
+  }
+
+  async onDelete(): Promise<void> {
+    const m = this.mapping();
+    if (!m) throw new Error('Mapping is undefined');
+    await this._service.delete(m.id!);
+    await this._router.navigate([this._locale.currentLocale()]);
+  }
+}
