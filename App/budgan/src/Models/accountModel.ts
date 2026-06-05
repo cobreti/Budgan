30ed8911cf8df30566d11699f16e021fd2182b26
Danny Thibaudeau <cobreti_@hotmail.com>
2026-06-05 19:12:02 -0400

    added reference balance in account

--- App/budgan/src/Models/accountModel.ts
+++ App/budgan/src/Models/accountModel.ts
@@ -2,4 +2,10 @@ export interface AccountModel {
   id: string;
   name: string;
   columnsMappingId: string;
+  referenceBalance?: AccountReferenceBalance;
+}
+
+export interface AccountReferenceBalance {
+  date: string;
+  balance: number;
 }
