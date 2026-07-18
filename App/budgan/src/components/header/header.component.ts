607f0bb5819e12b8a0bbd467bfe84f054c9590da
Danny Thibaudeau <cobreti_@hotmail.com>
2026-07-18 15:46:11 -0400

    added icon indicating if the application run in pwa or cloud mode

--- App/budgan/src/components/header/header.component.ts
+++ App/budgan/src/components/header/header.component.ts
@@ -5,18 +5,20 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatTooltip } from '@angular/material/tooltip';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MainMenuService } from '@services/main-menu.service';
 import { LOCALE_SERVICE } from '@services/locale.service';
 import { THEME_SERVICE } from '@services/theme.service';
 import { HeaderPageTitleComponent } from '@components/header/header-page-title/header-page-title.component';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatToolbar, HeaderPageTitleComponent, MatIconButton, MatIcon, MatMenu, MatMenuItem, MatMenuTrigger, TranslatePipe, NgOptimizedImage],
+  imports: [MatToolbar, HeaderPageTitleComponent, MatIconButton, MatIcon, MatMenu, MatMenuItem, MatMenuTrigger, MatTooltip, TranslatePipe, NgOptimizedImage],
 })
 export class HeaderComponent {
   private readonly _mainMenuService = inject(MainMenuService);
@@ -24,6 +26,7 @@ export class HeaderComponent {
   private readonly _router = inject(Router);
   private readonly _themeService = inject(THEME_SERVICE);
   protected readonly isDark = this._themeService.isDark;
+  protected readonly buildType = environment.buildType;
 
   onMenuBtnClick(): void {
     this._mainMenuService.toggleMenu();
