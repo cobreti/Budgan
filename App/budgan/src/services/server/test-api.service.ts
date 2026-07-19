36426a0b7de92625a855cc526171b1c1fad2835f
Danny Thibaudeau <cobreti_@hotmail.com>
2026-07-18 21:39:52 -0400

    added test api service

---
+++ App/budgan/src/services/server/test-api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, InjectionToken, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+export interface TestApiService {
+  test(): Promise<string>;
+}
+
+export const TEST_API_SERVICE = new InjectionToken<TestApiService>('TestApiService');
+
+@Injectable({ providedIn: 'root' })
+export class TestApiServiceImpl implements TestApiService {
+  private readonly _http = inject(HttpClient);
+
+  test(): Promise<string> {
+    return firstValueFrom(this._http.get('/api/Test', { responseType: 'text' }));
+  }
+}
