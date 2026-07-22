57939ef89b6877d30541332f5be2d0af8f323521
Danny Thibaudeau <cobreti_@hotmail.com>
2026-07-22 10:21:31 -0400

    updated look

--- App/budgan/src/views/accounts/account-graphs/account-graphs.component.ts
+++ App/budgan/src/views/accounts/account-graphs/account-graphs.component.ts
@@ -1,4 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle,
+} from '@angular/material/expansion';
+import { TranslatePipe } from '@ngx-translate/core';
 import { BalanceTrendGraphComponent } from '@views/accounts/account-graphs/balance-trend-graph/balance-trend-graph.component';
 import { RecurringPieChartComponent } from '@views/accounts/account-graphs/recurring-pie-chart/recurring-pie-chart.component';
 
@@ -7,7 +14,15 @@ import { RecurringPieChartComponent } from '@views/accounts/account-graphs/recur
   templateUrl: './account-graphs.component.html',
   styleUrl: './account-graphs.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [BalanceTrendGraphComponent, RecurringPieChartComponent],
+  imports: [
+    BalanceTrendGraphComponent,
+    RecurringPieChartComponent,
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle,
+    TranslatePipe,
+  ],
 })
 export class AccountGraphsComponent {
   readonly accountId = input.required<string>();
